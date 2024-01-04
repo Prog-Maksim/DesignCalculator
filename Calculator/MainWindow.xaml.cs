fe4aaaa65cf3ff5292b2ed4e3d84bfe5da10794c
Prog-Maksim <studentsbeloglazov2022@gmail.com>
2024-01-04 17:47:48 +0300

    Code Calculator

---
+++ Calculator/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows.Input;
+
+namespace Calculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        
+        private void Border_MouseDows(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                DragMove();
+        }
+    }
+}
